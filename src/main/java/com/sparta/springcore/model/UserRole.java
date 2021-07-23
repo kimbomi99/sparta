85f9dc6076fb66ef76e9b333a9cd4a7dd858384e
kimbomi99 <a99150316@gmail.com>
2021-07-23 13:08:13 +0900

    3주차 수업 끝!

--- src/main/java/com/sparta/springcore/model/UserRole.java
+++ src/main/java/com/sparta/springcore/model/UserRole.java
@@ -1,2 +1,6 @@
-package com.sparta.springcore.model;public class UserRole {
+package com.sparta.springcore.model;
+
+public enum UserRole {
+    USER,  // 사용자 권한
+    ADMIN  // 관리자 권한
 }
--- src/main/java/com/sparta/springcore/security/kakao/KakaoUserInfo.java
+++ src/main/java/com/sparta/springcore/security/kakao/KakaoUserInfo.java
@@ -1,4 +1,12 @@
-package com.sparta.springcore.security;
+package com.sparta.springcore.security.kakao;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class KakaoUserInfo {
+    Long id;
+    String email;
+    String nickname;
 }
